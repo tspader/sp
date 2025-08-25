1e40c1feb6272d32149521df7e720f1cb51de9c0
Thomas Spader <thomas.spader@gmail.com>
2025-08-24 23:57:36 -0400

    sp_format_hash_short

--- sp.h
+++ sp.h
@@ -734,6 +734,7 @@ typedef struct sp_formatter {
 #define SP_FMT_C16(V)           SP_FMT_ARG(c16, V)
 #define SP_FMT_CONTEXT(V)       SP_FMT_ARG(context, V)
 #define SP_FMT_HASH(V)          SP_FMT_ARG(hash, V)
+#define SP_FMT_SHORT_HASH(V)    SP_FMT_ARG(hash_short, V)
 #define SP_FMT_STR_BUILDER(V)   SP_FMT_ARG(str_builder, V)
 #define SP_FMT_DATE_TIME(V)     SP_FMT_ARG(date_time, V)
 #define SP_FMT_THREAD(V)        SP_FMT_ARG(thread, V)
@@ -760,6 +761,7 @@ void sp_format_c8(sp_str_builder_t* builder, sp_format_arg_t* buffer);
 void sp_format_c16(sp_str_builder_t* builder, sp_format_arg_t* buffer);
 void sp_format_context(sp_str_builder_t* builder, sp_format_arg_t* buffer);
 void sp_format_hash(sp_str_builder_t* builder, sp_format_arg_t* buffer);
+void sp_format_hash_short(sp_str_builder_t* builder, sp_format_arg_t* buffer);
 void sp_format_str_builder(sp_str_builder_t* builder, sp_format_arg_t* buffer);
 void sp_format_date_time(sp_str_builder_t* builder, sp_format_arg_t* buffer);
 void sp_format_thread(sp_str_builder_t* builder, sp_format_arg_t* buffer);
@@ -769,40 +771,46 @@ void sp_format_fixed_array(sp_str_builder_t* builder, sp_format_arg_t* buffer);
 void sp_format_dynamic_array(sp_str_builder_t* builder, sp_format_arg_t* buffer);
 void sp_format_quoted_str(sp_str_builder_t* builder, sp_format_arg_t* buffer);
 
+#define SP_BUILTIN_FORMATTERS \
+  SP_FORMATTER(ptr,           sp_format_ptr), \
+  SP_FORMATTER(str,           sp_format_str), \
+  SP_FORMATTER(cstr,          sp_format_cstr), \
+  SP_FORMATTER(s8,            sp_format_s8), \
+  SP_FORMATTER(s16,           sp_format_s16), \
+  SP_FORMATTER(s32,           sp_format_s32), \
+  SP_FORMATTER(s64,           sp_format_s64), \
+  SP_FORMATTER(u8,            sp_format_u8), \
+  SP_FORMATTER(u16,           sp_format_u16), \
+  SP_FORMATTER(u32,           sp_format_u32), \
+  SP_FORMATTER(u64,           sp_format_u64), \
+  SP_FORMATTER(f32,           sp_format_f32), \
+  SP_FORMATTER(f64,           sp_format_f64), \
+  SP_FORMATTER(c8,            sp_format_c8), \
+  SP_FORMATTER(c16,           sp_format_c16), \
+  SP_FORMATTER(context,       sp_format_context), \
+  SP_FORMATTER(hash,          sp_format_hash), \
+  SP_FORMATTER(hash_short,    sp_format_hash_short), \
+  SP_FORMATTER(str_builder,   sp_format_str_builder), \
+  SP_FORMATTER(date_time,     sp_format_date_time), \
+  SP_FORMATTER(thread,        sp_format_thread), \
+  SP_FORMATTER(mutex,         sp_format_mutex), \
+  SP_FORMATTER(semaphore,     sp_format_semaphore), \
+  SP_FORMATTER(fixed_array,   sp_format_fixed_array), \
+  SP_FORMATTER(dynamic_array, sp_format_dynamic_array), \
+  SP_FORMATTER(quoted_str,    sp_format_quoted_str), \
+
 #define SP_FMT(fmt, ...) sp_fmt(fmt, ##__VA_ARGS__);
 sp_str_t sp_fmt(sp_str_t fmt, ...);
 sp_str_t sp_fmt_c8(const c8* fmt, ...);
 sp_str_t sp_fmt_v(sp_str_t fmt, va_list args);
 
-#define SP_BUILTIN_FORMATTERS \
-  SP_FORMATTER(ptr, sp_format_ptr), \
-  SP_FORMATTER(str, sp_format_str), \
-  SP_FORMATTER(cstr, sp_format_cstr), \
-  SP_FORMATTER(s8, sp_format_s8), \
-  SP_FORMATTER(s16, sp_format_s16), \
-  SP_FORMATTER(s32, sp_format_s32), \
-  SP_FORMATTER(s64, sp_format_s64), \
-  SP_FORMATTER(u8, sp_format_u8), \
-  SP_FORMATTER(u16, sp_format_u16), \
-  SP_FORMATTER(u32, sp_format_u32), \
-  SP_FORMATTER(u64, sp_format_u64), \
-  SP_FORMATTER(f32, sp_format_f32), \
-  SP_FORMATTER(f64, sp_format_f64), \
-  SP_FORMATTER(c8, sp_format_c8), \
-  SP_FORMATTER(c16, sp_format_c16), \
-  SP_FORMATTER(context, sp_format_context), \
-  SP_FORMATTER(hash, sp_format_hash), \
-  SP_FORMATTER(str_builder, sp_format_str_builder), \
-  SP_FORMATTER(date_time, sp_format_date_time), \
-  SP_FORMATTER(thread, sp_format_thread), \
-  SP_FORMATTER(mutex, sp_format_mutex), \
-  SP_FORMATTER(semaphore, sp_format_semaphore), \
-  SP_FORMATTER(fixed_array, sp_format_fixed_array), \
-  SP_FORMATTER(dynamic_array, sp_format_dynamic_array), \
-  SP_FORMATTER(quoted_str, sp_format_quoted_str), \
 
-
-// parsers
+// ██████╗  █████╗ ██████╗ ███████╗███████╗██████╗ ███████╗
+// ██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔════╝██╔══██╗██╔════╝
+// ██████╔╝███████║██████╔╝███████╗█████╗  ██████╔╝███████╗
+// ██╔═══╝ ██╔══██║██╔══██╗╚════██║██╔══╝  ██╔══██╗╚════██║
+// ██║     ██║  ██║██║  ██║███████║███████╗██║  ██║███████║
+// ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝╚═╝  ╚═╝╚══════╝
 SP_API u8        sp_parse_u8(sp_str_t str);
 SP_API u16       sp_parse_u16(sp_str_t str);
 SP_API u32       sp_parse_u32(sp_str_t str);
@@ -836,6 +844,7 @@ SP_API bool      sp_parse_ptr_ex(sp_str_t str, void** out);
 SP_API bool      sp_parse_bool_ex(sp_str_t str, bool* out);
 SP_API bool      sp_parse_hash_ex(sp_str_t str, sp_hash_t* out);
 SP_API bool      sp_parse_hex_ex(sp_str_t str, u64* out);
+
 
 // ██╗      ██████╗  ██████╗
 // ██║     ██╔═══██╗██╔════╝
@@ -1977,7 +1986,7 @@ void sp_format_hex(sp_str_builder_t* builder, u64 value, u32 min_width, const c8
 
     while (value > 0) {
         u8 digit = value & 0xF;
-        hex_digits[digit_count++] = digit < 10 ? '0' + digit : 'A' + (digit - 10);
+        hex_digits[digit_count++] = digit < 10 ? '0' + digit : 'a' + (digit - 10);
         value >>= 4;
     }
 
@@ -2187,6 +2196,12 @@ void sp_format_hash(sp_str_builder_t* builder, sp_format_arg_t* arg) {
   sp_hash_t* value = (sp_hash_t*)arg->data;
   u64 hash = (u64)*value;
   sp_format_hex(builder, hash, 0, NULL);
+}
+
+void sp_format_hash_short(sp_str_builder_t* builder, sp_format_arg_t* arg) {
+  sp_hash_t* value = (sp_hash_t*)arg->data;
+  u64 hash = (u64)*value;
+  sp_format_hex(builder, hash >> 32, 0, NULL);
 }
 
 void sp_format_str_builder(sp_str_builder_t* builder, sp_format_arg_t* arg) {
