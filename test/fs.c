30ea35eaaf4232bfd399b0ded1b466477451e3f4
Thomas Spader <thomas.spader@gmail.com>
2025-11-24 22:52:45 -0500

    sp_fs: split out some tests

--- test/fs.c
+++ test/fs.c
@@ -177,7 +177,7 @@ UTEST_F(sp_test_fs, copy_preserves_file_attributes) {
   // Get original file stat information
   struct stat original_stat = {0};
   ASSERT_EQ(stat(source_cstr, &original_stat), 0);
-  
+
   // Copy the file
   sp_str_t copy_file = sp_test_file_path(&ut.file_manager, SP_LIT("copy_attrs.txt"));
   ASSERT_EQ(sp_os_copy(source_file, copy_file), SP_ERR_OK);
@@ -185,7 +185,7 @@ UTEST_F(sp_test_fs, copy_preserves_file_attributes) {
   // Verify copy exists and has same type
   ASSERT_TRUE(sp_os_does_path_exist(copy_file));
   ASSERT_TRUE(sp_os_is_regular_file(copy_file));
-  
+
   // Get copied file stat information
   const c8* copy_cstr = sp_str_to_cstr(copy_file);
   struct stat copy_stat = {0};
@@ -193,13 +193,294 @@ UTEST_F(sp_test_fs, copy_preserves_file_attributes) {
 
   // Verify file permissions (mode) are preserved
   ASSERT_EQ(original_stat.st_mode, copy_stat.st_mode);
-  
+
   // Verify file size is preserved
   ASSERT_EQ(original_stat.st_size, copy_stat.st_size);
-  
+
   // Verify content is identical
   sp_str_t copy_content = sp_io_read_file(copy_file);
   SP_EXPECT_STR_EQ(copy_content, test_content);
 }
+
+typedef struct {
+  sp_str_t file_path;
+  sp_str_t stem;
+} sp_test_file_stem_case_t;
+
+UTEST(path_functions, path_stem) {
+  sp_test_file_stem_case_t cases [] = {
+    {
+      .file_path = SP_LIT("foo.bar"),
+      .stem = SP_LIT("foo")
+    },
+    {
+      .file_path = SP_LIT("foo."),
+      .stem = SP_LIT("foo")
+    },
+    {
+      .file_path = SP_LIT("foo.bar.baz"),
+      .stem = SP_LIT("foo.bar")
+    },
+    {
+      .file_path = SP_LIT("foo"),
+      .stem = SP_LIT("foo")
+    },
+    {
+      .file_path = SP_LIT("foo.bar."),
+      .stem = SP_LIT("foo.bar")
+    },
+    {
+      .file_path = SP_LIT(".foo"),
+      .stem = SP_LIT("")
+    },
+  };
+
+  SP_CARR_FOR(cases, index) {
+    sp_str_t stem = sp_os_extract_stem(cases[index].file_path);
+    SP_EXPECT_STR_EQ(stem, cases[index].stem);
+  }
+}
+
+UTEST(path_functions, normalize_path) {
+  {
+    sp_str_t path = SP_LIT("C:\\Users\\Test\\file.txt");
+    sp_str_t copy = sp_os_normalize_path(path);
+    SP_EXPECT_STR_EQ_CSTR(copy, "C:/Users/Test/file.txt");
+  }
+
+  {
+    sp_str_t path = SP_LIT("C:/Users/Test/file.txt");
+    sp_str_t copy = sp_os_normalize_path(path);
+    SP_EXPECT_STR_EQ_CSTR(copy, "C:/Users/Test/file.txt");
+  }
+
+  {
+    sp_str_t path = SP_LIT("C:/Users\\Test/sub\\file.txt");
+    sp_str_t copy = sp_os_normalize_path(path);
+    SP_EXPECT_STR_EQ_CSTR(copy, "C:/Users/Test/sub/file.txt");
+  }
+
+  {
+    sp_str_t path = SP_LIT("");
+    sp_str_t copy = sp_os_normalize_path(path);
+    SP_EXPECT_STR_EQ_CSTR(copy, "");
+  }
+
+  {
+    sp_str_t path = SP_LIT("C:\\Users\\Test\\");
+    sp_str_t copy = sp_os_normalize_path(path);
+    SP_EXPECT_STR_EQ_CSTR(copy, "C:/Users/Test");
+  }
+}
+
+UTEST(path_functions, parent_path) {
+  {
+    sp_str_t path = SP_LIT("C:/Users/Test/file.txt");
+    sp_str_t parent = sp_os_parent_path(path);
+    SP_EXPECT_STR_EQ_CSTR(parent, "C:/Users/Test");
+  }
+
+  {
+    sp_str_t path = SP_LIT("C:/Users/Test/");
+    sp_str_t parent = sp_os_parent_path(path);
+    SP_EXPECT_STR_EQ_CSTR(parent, "C:/Users");
+  }
+
+  {
+    sp_str_t path = SP_LIT("C:/Users/Test///");
+    sp_str_t parent = sp_os_parent_path(path);
+    SP_EXPECT_STR_EQ_CSTR(parent, "C:/Users");
+  }
+
+  {
+    sp_str_t path = SP_LIT("C:/");
+    sp_str_t parent = sp_os_parent_path(path);
+    ASSERT_EQ(parent.len, 0);
+  }
+
+  {
+    sp_str_t path = SP_LIT("Test");
+    sp_str_t parent = sp_os_parent_path(path);
+    ASSERT_EQ(parent.len, 0);
+  }
+
+  {
+    sp_str_t path = SP_LIT("");
+    sp_str_t parent = sp_os_parent_path(path);
+    ASSERT_EQ(parent.len, 0);
+  }
+
+  {
+    sp_str_t path = SP_LIT("/");
+    sp_str_t parent = sp_os_parent_path(path);
+    ASSERT_EQ(parent.len, 0);
+  }
+
+  {
+    sp_str_t path = SP_LIT("/home/user/file");
+    sp_str_t parent = sp_os_parent_path(path);
+    SP_EXPECT_STR_EQ_CSTR(parent, "/home/user");
+  }
+}
+
+UTEST(path_functions, canonicalize_path) {
+  {
+    sp_str_t path = SP_LIT("test/..");
+    sp_str_t canonical = sp_os_canonicalize_path(path);
+    ASSERT_GT(canonical.len, 0);
+    ASSERT_NE(canonical.data[canonical.len - 1], '/');
+  }
+
+  {
+    sp_str_t path = SP_LIT("../../another");
+    sp_str_t canonical = sp_os_canonicalize_path(path);
+    ASSERT_GT(canonical.len, 0);
+    sp_str_t filename = sp_os_extract_file_name(canonical);
+    SP_EXPECT_STR_EQ_CSTR(filename, "another");
+  }
+
+  {
+    sp_str_t exe = sp_os_get_executable_path();
+    sp_str_t canonical = sp_os_canonicalize_path(exe);
+    ASSERT_TRUE(sp_str_equal(canonical, exe));
+  }
+
+  {
+    sp_str_t path = SP_LIT("test/");
+    sp_str_t canonical = sp_os_canonicalize_path(path);
+    ASSERT_GT(canonical.len, 0);
+    ASSERT_NE(canonical.data[canonical.len - 1], '/');
+  }
+
+  {
+    sp_str_t path = SP_LIT("");
+    sp_str_t canonical = sp_os_canonicalize_path(path);
+    ASSERT_EQ(canonical.len, 0);
+  }
+}
+
+typedef struct {
+  sp_str_t file_path;
+  sp_str_t extension;
+} sp_test_file_extension_case_t;
+
+UTEST(path_functions, path_extension) {
+  sp_test_file_extension_case_t cases [] = {
+    {
+      .file_path = SP_LIT("foo.bar"),
+      .extension = SP_LIT("bar")
+    },
+    {
+      .file_path = SP_LIT("foo."),
+      .extension = SP_LIT("")
+    },
+    {
+      .file_path = SP_LIT("foo.bar.baz"),
+      .extension = SP_LIT("baz")
+    },
+    {
+      .file_path = SP_LIT("foo"),
+      .extension = SP_LIT("")
+    },
+    {
+      .file_path = SP_LIT("foo.bar."),
+      .extension = SP_LIT("")
+    },
+    {
+      .file_path = SP_LIT(".foo"),
+      .extension = SP_LIT("foo")
+    },
+  };
+
+  SP_CARR_FOR(cases, index) {
+    sp_str_t extension = sp_os_extract_extension(cases[index].file_path);
+    SP_EXPECT_STR_EQ(extension, cases[index].extension);
+  }
+}
+
+
+UTEST(path_functions, extract_file_name) {
+  {
+    sp_str_t path = SP_LIT("C:/Users/Test/file.txt");
+    sp_str_t filename = sp_os_extract_file_name(path);
+    SP_EXPECT_STR_EQ_CSTR(filename, "file.txt");
+  }
+
+  {
+    sp_str_t path = SP_LIT("C:/Users/Test/");
+    sp_str_t filename = sp_os_extract_file_name(path);
+    ASSERT_EQ(filename.len, 0);
+  }
+
+  {
+    sp_str_t path = SP_LIT("C:\\Users\\Test\\file.txt");
+    sp_str_t filename = sp_os_extract_file_name(path);
+    SP_EXPECT_STR_EQ_CSTR(filename, "file.txt");
+  }
+
+  {
+    sp_str_t path = SP_LIT("file.txt");
+    sp_str_t filename = sp_os_extract_file_name(path);
+    SP_EXPECT_STR_EQ_CSTR(filename, "file.txt");
+  }
+
+  {
+    sp_str_t path = SP_LIT("");
+    sp_str_t filename = sp_os_extract_file_name(path);
+    ASSERT_EQ(filename.len, 0);
+  }
+
+  {
+    sp_str_t path = SP_LIT("/home/user/document.pdf");
+    sp_str_t filename = sp_os_extract_file_name(path);
+    SP_EXPECT_STR_EQ_CSTR(filename, "document.pdf");
+  }
+}
+
+UTEST(path_functions, get_executable_path) {
+  sp_str_t exe_path = sp_os_get_executable_path();
+
+  ASSERT_GT(exe_path.len, 0);
+
+  bool has_backslash = false;
+  for (u32 i = 0; i < exe_path.len; i++) {
+    if (exe_path.data[i] == '\\') {
+      has_backslash = true;
+      break;
+    }
+  }
+  ASSERT_FALSE(has_backslash);
+
+  ASSERT_NE(exe_path.data[exe_path.len - 1], '/');
+
+  sp_str_t filename = sp_os_extract_file_name(exe_path);
+  ASSERT_GT(filename.len, 0);
+}
+
+UTEST(path_functions, integration_test) {
+  sp_str_t exe = sp_os_get_executable_path();
+  sp_str_t parent1 = sp_os_parent_path(exe);
+  sp_str_t parent2 = sp_os_parent_path(parent1);
+  sp_str_t parent3 = sp_os_parent_path(parent2);
+  sp_str_t install = sp_os_canonicalize_path(parent3);
+
+  ASSERT_GT(install.len, 0);
+  ASSERT_NE(install.data[install.len - 1], '/');
+
+  sp_str_builder_t builder = SP_ZERO_INITIALIZE();
+  sp_str_builder_append(&builder, install);
+  sp_str_builder_append(&builder, SP_LIT("/build/space-dll.bat"));
+  sp_str_t dll_path = sp_str_builder_write(&builder);
+
+  bool has_double_slash = false;
+  for (u32 i = 1; i < dll_path.len; i++) {
+    if (dll_path.data[i-1] == '/' && dll_path.data[i] == '/') {
+      has_double_slash = true;
+      break;
+    }
+  }
+  ASSERT_FALSE(has_double_slash);
+}
+
 
 UTEST_MAIN()
