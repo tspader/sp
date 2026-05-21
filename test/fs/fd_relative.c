c901153bcc111186a7311ddf24752f10731526fd
Thomas Spader <thomas.spader@gmail.com>
2026-05-20 23:13:28 -0400

    test: clean up FS tests for gcc 13

--- test/fs/fd_relative.c
+++ test/fs/fd_relative.c
@@ -1,204 +1,185 @@
 #include "fs.h"
 
-static sp_sys_fd_t fd_rel_open_dir(sp_str_t path) {
-  return sp_sys_open_s(sp_sys_get_root(0), path, SP_O_RDONLY | SP_O_DIRECTORY, 0);
+typedef enum {
+  FD_REL_STAT,
+  FD_REL_OPEN,
+  FD_REL_RENAME,
+  FD_REL_LINK,
+} fd_rel_op_t;
+
+typedef struct {
+  const c8* label;
+  fs_setup_t setup[16];
+  const c8* dir;            // directory opened to obtain the relative fd
+  fd_rel_op_t op;
+  const c8* path;           // STAT/OPEN target, or RENAME/LINK source (relative to dir_fd)
+  const c8* dst;            // RENAME/LINK destination (relative to dir_fd)
+  bool absolute;            // OPEN: join path against the sandbox so the absolute path ignores the fd
+  const c8* expect_content; // OPEN: file contents read back through the fd
+  fs_expected_path_t expected[16];
+} fd_rel_test_t;
+
+static void run_fd_rel_test(s32* utest_result, sp_test_file_manager_t* fm, fd_rel_test_t t) {
+  sp_mem_t a = fm->mem;
+  sp_str_t sandbox = sp_test_file_path(fm, sp_str_view(t.label));
+  sp_fs_create_dir(sandbox);
+  fs_apply_setup(utest_result, fm, sandbox, t.setup);
+
+  sp_str_t dir = sp_fs_join_path(a, sandbox, sp_str_view(t.dir));
+  sp_sys_fd_t dir_fd = sp_sys_open_s(sp_sys_get_root(0), dir, SP_O_RDONLY | SP_O_DIRECTORY, 0);
+  ASSERT_NE(dir_fd, SP_SYS_INVALID_FD);
+
+  switch (t.op) {
+    case FD_REL_STAT: {
+      sp_sys_file_meta_t st = sp_zero;
+      EXPECT_EQ(sp_sys_get_path_metadata_s(dir_fd, sp_str_view(t.path), &st), 0);
+      break;
+    }
+    case FD_REL_OPEN: {
+      sp_str_t path = t.absolute
+        ? sp_fs_join_path(a, sandbox, sp_str_view(t.path))
+        : sp_str_view(t.path);
+      sp_sys_fd_t file_fd = sp_sys_open_s(dir_fd, path, SP_O_RDONLY | SP_O_BINARY, 0);
+      EXPECT_NE(file_fd, SP_SYS_INVALID_FD);
+      if (file_fd != SP_SYS_INVALID_FD) {
+        if (t.expect_content) {
+          c8 buf[64] = sp_zero;
+          s64 n = sp_sys_read(file_fd, buf, sizeof(buf));
+          SP_EXPECT_STR_EQ_CSTR(sp_str(buf, n < 0 ? 0 : (u32)n), t.expect_content);
+        }
+        sp_sys_close(file_fd);
+      }
+      break;
+    }
+    case FD_REL_RENAME: {
+      EXPECT_EQ(sp_sys_rename_s(dir_fd, sp_str_view(t.path), dir_fd, sp_str_view(t.dst)), 0);
+      break;
+    }
+    case FD_REL_LINK: {
+      EXPECT_EQ(sp_sys_link_s(dir_fd, sp_str_view(t.path), dir_fd, sp_str_view(t.dst)), 0);
+      break;
+    }
+  }
+
+  sp_sys_close(dir_fd);
+  fs_expect_paths(utest_result, fm, sandbox, t.expected);
 }
 
 UTEST_F(fs, fd_relative_stat_dotdot) {
   SKIP_ON_WASM()
-  sp_mem_t a = ut.file_manager.mem;
-  sp_str_t sandbox = sp_test_file_path(&ut.file_manager, sp_str_lit("fd_relative_stat_dotdot"));
-  sp_fs_create_dir(sandbox);
-  fs_apply_setup(&ur, &ut.file_manager, sandbox, (fs_setup_t[]) {
-    { .path = "a/file.txt", .kind = FS_SETUP_FILE, .content = "hello" },
-    { 0 },
+  run_fd_rel_test(&ur, &ut.file_manager, (fd_rel_test_t) {
+    .label = "fd_relative_stat_dotdot",
+    .setup = {
+      { .path = "a/file.txt", .kind = FS_SETUP_FILE, .content = "hello" },
+    },
+    .dir = "a",
+    .op = FD_REL_STAT,
+    .path = "../a/file.txt",
   });
-
-  sp_str_t dir_a = sp_fs_join_path(a, sandbox, sp_str_lit("a"));
-  sp_sys_fd_t dir_fd = fd_rel_open_dir(dir_a);
-  ASSERT_NE(dir_fd, SP_SYS_INVALID_FD);
-
-  sp_sys_file_meta_t st = sp_zero;
-  s32 rc = sp_sys_get_path_metadata_s(dir_fd, sp_str_lit("../a/file.txt"), &st);
-  EXPECT_EQ(rc, 0);
-
-  sp_sys_close(dir_fd);
 }
 
 UTEST_F(fs, fd_relative_open_dotdot) {
   SKIP_ON_WASM()
-  sp_mem_t a = ut.file_manager.mem;
-  sp_str_t sandbox = sp_test_file_path(&ut.file_manager, sp_str_lit("fd_relative_open_dotdot"));
-  sp_fs_create_dir(sandbox);
-  fs_apply_setup(&ur, &ut.file_manager, sandbox, (fs_setup_t[]) {
-    { .path = "a/file.txt", .kind = FS_SETUP_FILE, .content = "hello" },
-    { 0 },
+  run_fd_rel_test(&ur, &ut.file_manager, (fd_rel_test_t) {
+    .label = "fd_relative_open_dotdot",
+    .setup = {
+      { .path = "a/file.txt", .kind = FS_SETUP_FILE, .content = "hello" },
+    },
+    .dir = "a",
+    .op = FD_REL_OPEN,
+    .path = "../a/file.txt",
+    .expect_content = "hello",
   });
-
-  sp_str_t dir_a = sp_fs_join_path(a, sandbox, sp_str_lit("a"));
-  sp_sys_fd_t dir_fd = fd_rel_open_dir(dir_a);
-  ASSERT_NE(dir_fd, SP_SYS_INVALID_FD);
-
-  sp_sys_fd_t file_fd = sp_sys_open_s(dir_fd, sp_str_lit("../a/file.txt"), SP_O_RDONLY | SP_O_BINARY, 0);
-  EXPECT_NE(file_fd, SP_SYS_INVALID_FD);
-
-  if (file_fd != SP_SYS_INVALID_FD) {
-    c8 buf[16] = sp_zero;
-    s64 n = sp_sys_read(file_fd, buf, sizeof(buf));
-    EXPECT_EQ((s32)n, 5);
-    EXPECT_EQ(buf[0], 'h');
-    EXPECT_EQ(buf[4], 'o');
-    sp_sys_close(file_fd);
-  }
-
-  sp_sys_close(dir_fd);
 }
 
 UTEST_F(fs, fd_relative_rename_dotdot) {
   SKIP_ON_WASM()
-  sp_mem_t a = ut.file_manager.mem;
-  sp_str_t sandbox = sp_test_file_path(&ut.file_manager, sp_str_lit("fd_relative_rename_dotdot"));
-  sp_fs_create_dir(sandbox);
-  fs_apply_setup(&ur, &ut.file_manager, sandbox, (fs_setup_t[]) {
-    { .path = "a/file.txt", .kind = FS_SETUP_FILE, .content = "hello" },
-    { 0 },
+  run_fd_rel_test(&ur, &ut.file_manager, (fd_rel_test_t) {
+    .label = "fd_relative_rename_dotdot",
+    .setup = {
+      { .path = "a/file.txt", .kind = FS_SETUP_FILE, .content = "hello" },
+    },
+    .dir = "a",
+    .op = FD_REL_RENAME,
+    .path = "file.txt",
+    .dst = "../a/file2.txt",
+    .expected = {
+      { .path = "a/file.txt",  .exists = FS_EXPECT_NOT_EXIST, .attr = SP_FS_KIND_NONE },
+      { .path = "a/file2.txt", .exists = FS_EXPECT_EXIST,     .attr = SP_FS_KIND_FILE },
+    },
   });
-
-  sp_str_t dir_a = sp_fs_join_path(a, sandbox, sp_str_lit("a"));
-  sp_sys_fd_t dir_fd = fd_rel_open_dir(dir_a);
-  ASSERT_NE(dir_fd, SP_SYS_INVALID_FD);
-
-  s32 rc = sp_sys_rename_s(dir_fd, sp_str_lit("file.txt"), dir_fd, sp_str_lit("../a/file2.txt"));
-  EXPECT_EQ(rc, 0);
-
-  sp_str_t old_path = sp_fs_join_path(a, sandbox, sp_str_lit("a/file.txt"));
-  sp_str_t new_path = sp_fs_join_path(a, sandbox, sp_str_lit("a/file2.txt"));
-  EXPECT_FALSE(sp_fs_exists(old_path));
-  EXPECT_TRUE(sp_fs_exists(new_path));
-
-  sp_sys_close(dir_fd);
 }
 
 UTEST_F(fs, fd_relative_link_dotdot) {
   SKIP_ON_WASM()
-  sp_mem_t a = ut.file_manager.mem;
-  sp_str_t sandbox = sp_test_file_path(&ut.file_manager, sp_str_lit("fd_relative_link_dotdot"));
-  sp_fs_create_dir(sandbox);
-  fs_apply_setup(&ur, &ut.file_manager, sandbox, (fs_setup_t[]) {
-    { .path = "a/file.txt", .kind = FS_SETUP_FILE, .content = "hello" },
-    { 0 },
+  run_fd_rel_test(&ur, &ut.file_manager, (fd_rel_test_t) {
+    .label = "fd_relative_link_dotdot",
+    .setup = {
+      { .path = "a/file.txt", .kind = FS_SETUP_FILE, .content = "hello" },
+    },
+    .dir = "a",
+    .op = FD_REL_LINK,
+    .path = "file.txt",
+    .dst = "../a/file.hard",
+    .expected = {
+      { .path = "a/file.hard", .exists = FS_EXPECT_EXIST, .attr = SP_FS_KIND_FILE },
+    },
   });
-
-  sp_str_t dir_a = sp_fs_join_path(a, sandbox, sp_str_lit("a"));
-  sp_sys_fd_t dir_fd = fd_rel_open_dir(dir_a);
-  ASSERT_NE(dir_fd, SP_SYS_INVALID_FD);
-
-  s32 rc = sp_sys_link_s(dir_fd, sp_str_lit("file.txt"), dir_fd, sp_str_lit("../a/file.hard"));
-  EXPECT_EQ(rc, 0);
-
-  sp_str_t link_path = sp_fs_join_path(a, sandbox, sp_str_lit("a/file.hard"));
-  EXPECT_TRUE(sp_fs_exists(link_path));
-
-  sp_sys_close(dir_fd);
 }
 
 UTEST_F(fs, fd_relative_open_mixed_separators) {
   SKIP_UNLESS_WIN32()
-  sp_mem_t a = ut.file_manager.mem;
-  sp_str_t sandbox = sp_test_file_path(&ut.file_manager, sp_str_lit("fd_relative_open_mixed_separators"));
-  sp_fs_create_dir(sandbox);
-  fs_apply_setup(&ur, &ut.file_manager, sandbox, (fs_setup_t[]) {
-    { .path = "a/file.txt", .kind = FS_SETUP_FILE, .content = "hello" },
-    { 0 },
+  run_fd_rel_test(&ur, &ut.file_manager, (fd_rel_test_t) {
+    .label = "fd_relative_open_mixed_separators",
+    .setup = {
+      { .path = "a/file.txt", .kind = FS_SETUP_FILE, .content = "hello" },
+    },
+    .dir = "a",
+    .op = FD_REL_OPEN,
+    .path = "..\\a/file.txt",
   });
-
-  sp_str_t dir_a = sp_fs_join_path(a, sandbox, sp_str_lit("a"));
-  sp_sys_fd_t dir_fd = fd_rel_open_dir(dir_a);
-  ASSERT_NE(dir_fd, SP_SYS_INVALID_FD);
-
-  sp_sys_fd_t file_fd = sp_sys_open_s(dir_fd, sp_str_lit("..\\a/file.txt"), SP_O_RDONLY | SP_O_BINARY, 0);
-  EXPECT_NE(file_fd, SP_SYS_INVALID_FD);
-  if (file_fd != SP_SYS_INVALID_FD) sp_sys_close(file_fd);
-
-  sp_sys_close(dir_fd);
 }
 
 UTEST_F(fs, fd_relative_open_dot_segments) {
   SKIP_ON_WASM()
-  sp_mem_t a = ut.file_manager.mem;
-  sp_str_t sandbox = sp_test_file_path(&ut.file_manager, sp_str_lit("fd_relative_open_dot_segments"));
-  sp_fs_create_dir(sandbox);
-  fs_apply_setup(&ur, &ut.file_manager, sandbox, (fs_setup_t[]) {
-    { .path = "a/file.txt", .kind = FS_SETUP_FILE, .content = "hello" },
-    { 0 },
+  run_fd_rel_test(&ur, &ut.file_manager, (fd_rel_test_t) {
+    .label = "fd_relative_open_dot_segments",
+    .setup = {
+      { .path = "a/file.txt", .kind = FS_SETUP_FILE, .content = "hello" },
+    },
+    .dir = "a",
+    .op = FD_REL_OPEN,
+    .path = "./././file.txt",
   });
-
-  sp_str_t dir_a = sp_fs_join_path(a, sandbox, sp_str_lit("a"));
-  sp_sys_fd_t dir_fd = fd_rel_open_dir(dir_a);
-  ASSERT_NE(dir_fd, SP_SYS_INVALID_FD);
-
-  sp_sys_fd_t file_fd = sp_sys_open_s(dir_fd, sp_str_lit("./././file.txt"), SP_O_RDONLY | SP_O_BINARY, 0);
-  EXPECT_NE(file_fd, SP_SYS_INVALID_FD);
-  if (file_fd != SP_SYS_INVALID_FD) sp_sys_close(file_fd);
-
-  sp_sys_close(dir_fd);
 }
 
 UTEST_F(fs, fd_relative_open_absolute_ignores_fd) {
   SKIP_ON_WASM()
-  sp_mem_t a = ut.file_manager.mem;
-  sp_str_t sandbox = sp_test_file_path(&ut.file_manager, sp_str_lit("fd_relative_open_absolute_ignores_fd"));
-  sp_fs_create_dir(sandbox);
-  fs_apply_setup(&ur, &ut.file_manager, sandbox, (fs_setup_t[]) {
-    { .path = "a/file.txt", .kind = FS_SETUP_FILE, .content = "hello" },
-    { .path = "b/file.txt", .kind = FS_SETUP_FILE, .content = "world" },
-    { 0 },
+  run_fd_rel_test(&ur, &ut.file_manager, (fd_rel_test_t) {
+    .label = "fd_relative_open_absolute_ignores_fd",
+    .setup = {
+      { .path = "a/file.txt", .kind = FS_SETUP_FILE, .content = "hello" },
+      { .path = "b/file.txt", .kind = FS_SETUP_FILE, .content = "world" },
+    },
+    .dir = "a",
+    .op = FD_REL_OPEN,
+    .path = "b/file.txt",
+    .absolute = true,
+    .expect_content = "world",
   });
-
-  sp_str_t dir_a = sp_fs_join_path(a, sandbox, sp_str_lit("a"));
-  sp_sys_fd_t dir_fd = fd_rel_open_dir(dir_a);
-  ASSERT_NE(dir_fd, SP_SYS_INVALID_FD);
-
-  sp_str_t abs_b = sp_fs_join_path(a, sandbox, sp_str_lit("b/file.txt"));
-  sp_sys_fd_t file_fd = sp_sys_open_s(dir_fd, abs_b, SP_O_RDONLY | SP_O_BINARY, 0);
-  EXPECT_NE(file_fd, SP_SYS_INVALID_FD);
-
-  if (file_fd != SP_SYS_INVALID_FD) {
-    c8 buf[16] = sp_zero;
-    s64 n = sp_sys_read(file_fd, buf, sizeof(buf));
-    EXPECT_EQ((s32)n, 5);
-    EXPECT_EQ(buf[0], 'w');
-    sp_sys_close(file_fd);
-  }
-
-  sp_sys_close(dir_fd);
 }
 
 UTEST_F(fs, fd_relative_open_deep_dotdot) {
   SKIP_ON_WASM()
-  sp_mem_t a = ut.file_manager.mem;
-  sp_str_t sandbox = sp_test_file_path(&ut.file_manager, sp_str_lit("fd_relative_open_deep_dotdot"));
-  sp_fs_create_dir(sandbox);
-  fs_apply_setup(&ur, &ut.file_manager, sandbox, (fs_setup_t[]) {
-    { .path = "a/b/inner.txt", .kind = FS_SETUP_FILE, .content = "deep" },
-    { .path = "a/sibling.txt", .kind = FS_SETUP_FILE, .content = "side" },
-    { 0 },
+  run_fd_rel_test(&ur, &ut.file_manager, (fd_rel_test_t) {
+    .label = "fd_relative_open_deep_dotdot",
+    .setup = {
+      { .path = "a/b/inner.txt", .kind = FS_SETUP_FILE, .content = "deep" },
+      { .path = "a/sibling.txt", .kind = FS_SETUP_FILE, .content = "side" },
+    },
+    .dir = "a/b",
+    .op = FD_REL_OPEN,
+    .path = "../sibling.txt",
+    .expect_content = "side",
   });
-
-  sp_str_t dir_b = sp_fs_join_path(a, sandbox, sp_str_lit("a/b"));
-  sp_sys_fd_t dir_fd = fd_rel_open_dir(dir_b);
-  ASSERT_NE(dir_fd, SP_SYS_INVALID_FD);
-
-  sp_sys_fd_t file_fd = sp_sys_open_s(dir_fd, sp_str_lit("../sibling.txt"), SP_O_RDONLY | SP_O_BINARY, 0);
-  EXPECT_NE(file_fd, SP_SYS_INVALID_FD);
-
-  if (file_fd != SP_SYS_INVALID_FD) {
-    c8 buf[16] = sp_zero;
-    s64 n = sp_sys_read(file_fd, buf, sizeof(buf));
-    EXPECT_EQ((s32)n, 4);
-    EXPECT_EQ(buf[0], 's');
-    sp_sys_close(file_fd);
-  }
-
-  sp_sys_close(dir_fd);
 }
