629d5e77f622909a545de25aa407c390c1372054
Thomas Spader <thomas.spader@gmail.com>
2026-07-27 19:20:19 -0400

    test: fix golden root detection

--- test/main.c
+++ test/main.c
@@ -1,11 +1,6 @@
 #define SP_IMPLEMENTATION
 #include "sp/sp_test.h"
 
-// #include "sp_test/filter.c"
-// #include "sp_test/resolve.c"
-// #include "sys/open.c"
-// #include "sys/iter.c"
-//
 static s32 entry(s32 argc, const c8** argv) {
   return sp_test_main(argc, argv, SP_NULLPTR);
 }
