80d1aad696a0e3f0313312a6b149badc114bc6ac
Thomas Spader <thomas.spader@gmail.com>
2026-08-30 14:17:39 -0400

    blocking backend

--- test/io/ops/new.c
+++ test/io/ops/new.c
@@ -11,9 +11,11 @@ sp_test(io, new_yields_usable_default) {
   sp_io_op_t op = { .kind = SP_IO_OP_TIMEOUT, .timeout = { .timeout = sp_io_timeout_after(0) } };
   sp_must_ok(t, sp_io_submit(io, &op));
 
-  u32 count = 0;
-  sp_expect_ok(t, sp_io_dispatch(io, sp_io_timeout_after(sp_tm_ms_to_ns(OPS_WAIT_MS)), &count));
-  sp_expect_eq(t, count, (u32)1);
+  if (!op.done) {
+    u32 count = 0;
+    sp_expect_ok(t, sp_io_dispatch(io, sp_io_timeout_after(sp_tm_ms_to_ns(OPS_WAIT_MS)), &count));
+    sp_expect_eq(t, count, (u32)1);
+  }
   sp_expect_eq(t, op.done, true);
   sp_expect_ok(t, op.result.err);
 
